d215fe054a514c01f1cafefd86f099e8c4614feb
Will Cass <will.cass@ovic.co.uk>
2015-12-04 17:19:21 +0000

    Tweaking the golf and standard scripts

--- Day 4/day4pt2.pl
+++ Day 4/day4pt2.pl
@@ -1,10 +1,8 @@
 use strict;
 use warnings;
 use Digest::MD5 qw(md5 md5_hex md5_base64);
-
 my $string = 'bgvyzdsv';
 my $i = 0;
-
 while($i<10000000){
 	$i++;
 	my $data = "$string$i";
@@ -13,7 +11,5 @@ while($i<10000000){
 		print "$digest - $data - $i\n";
 		last;
 	}
-	# print "$digest - $data - $i\n";
 }
-
 print $i;
