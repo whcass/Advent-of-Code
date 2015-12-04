9a0809684665dc3f7642a1899fe5eb91fc01ba86
Will Cass <will.cass@ovic.co.uk>
2015-12-04 11:20:34 +0000

    Day 4 done!
    Turns out, if you're printing useless information it takes a while!

--- Day 4/day4pt2.pl
+++ Day 4/day4pt2.pl
@@ -5,15 +5,15 @@ use Digest::MD5 qw(md5 md5_hex md5_base64);
 my $string = 'bgvyzdsv';
 my $i = 0;
 
-while($i<1000000){
+while($i<10000000){
 	$i++;
 	my $data = "$string$i";
 	my $digest = md5_hex($data);
-	if($digest =~ /^0{5,}/){
+	if($digest =~ /^0{6,}/){
 		print "$digest - $data - $i\n";
 		last;
 	}
-	print "$digest - $data - $i\n";
+	# print "$digest - $data - $i\n";
 }
 
 print $i;
