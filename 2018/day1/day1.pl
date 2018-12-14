4ed8961cc3d3e58e3fa8493d1350896c14572df2
Will Cass <will_cass@hotmail.co.uk>
2018-12-14 02:06:23 +0000

    Day 1 done

--- 2018/day1/day1.pl
+++ 2018/day1/day1.pl
@@ -1,2 +1,14 @@
 #!/usr/bin/perl
+#!/usr/bin/perl
+use strict;
+use warnings;
+my $file = "day1.txt";
+open my $data, $file, or die "Could not open $file: $!";
+my $output = 0;
+while(my $line = <$data>){
+    my @matches = $line =~ /([\+|-])(\d+)/;
+    $output += $line;
+    
+}
 
+print "[*] $output";
