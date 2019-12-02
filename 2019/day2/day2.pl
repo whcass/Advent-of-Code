2064db1ac687954e6766300523c4ded75382e073
Will Cass <will_cass@hotmail.co.uk>
2019-12-02 10:05:16 +0000

    Day2 part 1 complete!

---
+++ 2019/day2/day2.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $file = "day2.txt";
+open my $data, $file or die "Could not open file: $!";
+my @code = split /,/,<$data>;
+#print "@code[0]\n";
+$code[1] = 12;
+$code[2] = 2;
+my $ptr = 0;
+while (1){
+	my $inst = $code[$ptr];
+	if($inst == 99)
+	{
+		last;
+	}elsif($inst == 1)
+	{
+		my $first = $code[$code[$ptr+1]];
+		my $second = $code[$code[$ptr+2]];
+		my $out = $code[$ptr+3];
+		print "$first : $second : $out\n";
+		$code[$out] = $first+$second;
+	}elsif($inst == 2)
+	{
+		my $first = $code[$code[$ptr+1]];
+		my $second = $code[$code[$ptr+2]];
+		my $out = $code[$ptr+3];
+		#print "OUT:$out\n";
+		print "$first : $second : $out\n";
+		$code[$out] = $first*$second;
+	}
+	$ptr+=4;
+}
+
+print "$code[0]\n";
