9e1d83fde680c28cded263b239c71986a93d5692
Will Cass <will_cass@hotmail.co.uk>
2019-12-04 23:14:13 +0000

    Day 2 done aswell in the same file

--- 2019/day3/day3.pl
+++ 2019/day3/day3.pl
@@ -17,42 +17,54 @@ sub BuildInstructions {
     return @res;
 }
 
-sub ShortestDistance {
-    my @list = @_;
+sub manhattanDistance {
+    my %list = @_;
     my $shortest = 999999999999999999;
-    foreach my $point (@list){
+    foreach my $point (keys %list){
         my @coord = split /,/,$point;
+        
         my $x = $coord[0];
         my $y = $coord[1];
         my $dist = abs($x)+abs($y);
         $shortest = $dist if $dist < $shortest;
     }
 
-    print $shortest;
+    print "$shortest\n";
+}
+
+sub signalDistance {
+    my %list = @_;
+    my $shortest = 9999999999;
+    foreach (keys %list){
+        $shortest = $list{$_} if $list{$_} < $shortest;
+        # print "$list{$_}\n";
+    }
+    print "$shortest\n";
 }
 
 sub findPath {
     my @inst = @_;
-    my @res;
+    my %res;
     my $x = 0;
     my $y = 0;
     for ( my $i = 0 ; $i < @inst ; $i++ ) {
+        my $steps = $i + 1;
         my $dir = $inst[$i];
         if($dir eq 'R'){
             $x++;
-            push(@res,"$x,$y");
+            $res{"$x,$y"} = $steps;
         }elsif($dir eq 'L'){
             $x--;
-            push(@res,"$x,$y");
+            $res{"$x,$y"} = $steps;
         }elsif($dir eq 'U'){
             $y++;
-            push(@res,"$x,$y");
+            $res{"$x,$y"} = $steps;
         }elsif($dir eq 'D'){
             $y--;
-            push(@res,"$x,$y");
+            $res{"$x,$y"} = $steps;
         }
     }
-    return @res;
+    return %res;
     # print("$res[0]\n");
 }
 
@@ -64,8 +76,14 @@ open my $wireb, $fileb or die "Could not open file: $!";
 my @one = split /,/, <$wirea>;
 my @two = split /,/, <$wireb>;
 
-@one = findPath( BuildInstructions(@one) );
-@two = findPath( BuildInstructions(@two) );
-my @intersect = intersect(@one,@two);
-ShortestDistance(@intersect);
-
+my %oneHash = findPath( BuildInstructions(@one) );
+my %twoHash = findPath( BuildInstructions(@two) );
+my %intersect;
+foreach(keys %oneHash){
+    if (exists $twoHash{$_}){
+        $intersect{$_} = $twoHash{$_}+$oneHash{$_};
+    }
+}
+# print Dumper(%intersect);
+manhattanDistance(%intersect);
+signalDistance(%intersect);
