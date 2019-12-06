bda94a08672646ccb71b406ff8fcd55723b25e50
Will Cass <will_cass@hotmail.co.uk>
2019-12-06 19:02:21 +0000

    part2 done!

--- 2019/day6/day6part2.pl
+++ 2019/day6/day6part2.pl
@@ -2,8 +2,6 @@ use strict;
 use warnings;
 no warnings 'recursion';
 use Data::Dumper;
-
-# use Data::TreeDumper;
 use Tree::Simple;
 
 my $input = "COM)B
@@ -17,46 +15,38 @@ D)I
 E)J
 J)K
 K)L";
-my $file = "part2test.txt";
+my $file = "day6.txt";
 open my $data, $file or die "Could not open file: $!";
 my %bodies;
 my $bodyCount = 0;
 foreach my $line (<$data>) {
 
-    # chomp $line;
     my ( $body, $orbiter ) = $line =~ /(.*)\)(.*)/;
 
-    # print "$orbiter orbits $body\n";
     if ( !$bodies{$body} ) {
-        # print "[*] body $body does not exist, making a node\n";
         $bodies{$body} = Tree::Simple->new( $body, Tree::Simple->ROOT );
         $bodies{$body}->addChild( Tree::Simple->new($orbiter) );
-        
     }
     else {
-        # print "[*] adding $orbiter to $body\n";
         $bodies{$body}->addChild( Tree::Simple->new($orbiter) );
     }
     $bodyCount++;
-    # my @children = @{$tree{$parent}{'children'}};
-    # $tree{$parent}{'children'} = push @children,$child;
 }
 
 my $root  = $bodies{'COM'};
 my $total = 0;
 
-# print Dumper(%bodies);
-# Globals are grim yo
-print "finding you\n";
 my @pathToYou = findPath($root,"YOU");
-print "finding santa\n";
 my @pathToSanta = findPath($root,'SAN');
-# print @pathToSanta;
-# print Dumper($bodies{'YOU'});
-# foreach my $child ( $root->getAllChildren ) {
-#     count( $child->getNodeValue, 0 );
-# }
-
+my %repeats;
+my @diff;
+for (@pathToYou,@pathToSanta){ $repeats{$_}++}
+for (keys %repeats){
+    push @diff, $_ unless $repeats{$_} > 1;
+}
+my $steps = @diff;
+$steps -= 2;
+print "[*] $steps\n";
 
 sub findPath
 {
@@ -65,19 +55,15 @@ sub findPath
 
     my ($ignore,@path) = findWho($root->getNodeValue,$who);
     @path = reverse(@path);
-    print "@path\n";
-    return 
+    return @path;
 }
 
-# Weird things happen in this function
 
 sub findWho
 {
-    # my @path = @{ $_[0] };
     my $node = $bodies{$_[0]};
     my $who = $_[1];
     my @path;
-    # my $bodyName = $node->getNodeValue;
     foreach my $child ($node->getAllChildren){
         if($bodies{$child->getNodeValue}){
             my($record,@path) = findWho($child->getNodeValue,$who);
@@ -92,27 +78,3 @@ sub findWho
         }
     }
 }
-
-sub count {
-    my $node  = $bodies{ $_[0] };
-    my $depth = $_[1];
-    $depth++;
-    
-    my $bodyName = $node->getNodeValue;
-
-    # print "[+] $bodyName - D:1 - I:$depth = $depth\n";
-    # print "body: $name\n";
-    foreach my $child ( $node->getAllChildren ) {
-        my $name = $child->getNodeValue;
-        print "[*] $name:$depth\n";
-        $total += $depth;
-        # print "[*] $bodyName has orbiting:$name\n\t$depth\n";
-        if ( $bodies{$name} ) {
-            count( $name, $depth );
-        }
-    }
-}
-# print "[*] indirects: $total\n";
-# print "[*] bodyCount: $bodyCount\n";
-# my $count = $total + $bodyCount;
-# print "[*] total: $count\n";
