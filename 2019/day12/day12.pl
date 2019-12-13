6b503a341b3da988fb55c356baa1f43e49bf9d5c
Will Cass <will_cass@hotmail.co.uk>
2019-12-13 15:49:51 +0000

    one vel in each planet is 1 off

--- 2019/day12/day12.pl
+++ 2019/day12/day12.pl
@@ -2,4 +2,41 @@ use strict;
 use warnings;
 use AdventOfCode::Planet;
 
-my $planet = AdventOfCode::Planet->new(8,0,8);
+# <x=8, y=0, z=8>
+# <x=0, y=-5, z=-10>
+# <x=16, y=10, z=-5>
+# <x=19, y=-10, z=-7>
+# my $A = AdventOfCode::Planet->new(8,0,8);
+# my $B = AdventOfCode::Planet->new(0,-5,-10);
+# my $C = AdventOfCode::Planet->new(16,10,-5);
+# my $D = AdventOfCode::Planet->new(19,-10,-7);
+# <x=-1, y=0, z=2>
+# <x=2, y=-10, z=-7>
+# <x=4, y=-8, z=8>
+# <x=3, y=5, z=-1>
+my $A = AdventOfCode::Planet->new( -1, 0,   2 );
+my $B = AdventOfCode::Planet->new( 2,  -10, -7 );
+my $C = AdventOfCode::Planet->new( 4,  -8,  8 );
+my $D = AdventOfCode::Planet->new( 3,  5,   -1 );
+my @otherPlanets = ( $A, $B, $C, $D );
+print "After 0 steps:\n";
+for (@otherPlanets) {
+    $_->printOut();
+}
+print "\n";
+for my $index ( 1 .. 2 ) {
+    my @temp = @otherPlanets;
+    print "After $index steps:\n";
+    shift @temp;
+    $A->update( \@temp );
+    shift @temp;
+    $B->update( \@temp );
+    shift @temp;
+    $C->update( \@temp );
+    shift @temp;
+    $D->update( \@temp );
+    for (@otherPlanets) {
+        $_->printOut();
+    }
+    print "\n";
+}
--- 2019/lib/AdventOfCode/Planet.pm
+++ 2019/lib/AdventOfCode/Planet.pm
@@ -3,22 +3,79 @@ use strict;
 use warnings;
 use Exporter qw(import);
 
-our @EXPORT_OK = qw(new);
+our @EXPORT_OK = qw(new update printOut);
+
 sub new {
     my $class = shift;
-    my $x = $_[0];
-    my $y = $_[1];
-    my $z = $_[2];
-    my %pos;
-    $pos{'x'} = $x;
-    $pos{'y'} = $y;
-    $pos{'z'} = $z;
-    my %vel;
-    $vel{'x'} = $vel{'y'} = $vel{'z'} = 0;
+    my $x     = $_[0];
+    my $y     = $_[1];
+    my $z     = $_[2];
+    my @pos;
+    $pos[0] = $x;
+    $pos[1] = $y;
+    $pos[2] = $z;
+    my @vel;
+    $vel[0] = $vel[1] = $vel[2] = 0;
+
+    # print $pos{'x'};
     my $self = bless {
-        pos=>%pos,
-        vel=>%vel,
+        pos => [@pos],
+        vel => [@vel],
     }, $class;
 
     return $self;
+}
+
+sub printOut {
+    my $self = shift;
+    my @pos  = @{ $self->@{pos} };
+    my @vel  = @{ $self->@{vel} };
+    print
+"pos=<x=$pos[0], y=$pos[1], z=$pos[2]>, vel=<x=$vel[0], y=$vel[1], z=$vel[2]>\n";
+}
+
+sub update {
+    my $self         = shift;
+    my @otherPlanets = @{ $_[0] };
+    my ( $lhsX, $lhsY, $lhsZ ) = @{ $self->@{pos} };
+    for (@otherPlanets) {
+        my $obj = $_;
+        if ( $self == $obj ) {
+            next;
+        }
+        my ( $rhsX, $rhsY, $rhsZ ) = @{ $obj->@{pos} };
+
+        # my @objvel = @{$obj->@{vel}};
+        if ( $lhsX > $rhsX ) {
+            @{ $self->@{vel} }[0]--;
+            @{ $obj->@{vel} }[0]++;
+        }
+        else {
+            @{ $self->@{vel} }[0]++;
+            @{ $obj->@{vel} }[0]--;
+        }
+
+        if ( $lhsY > $rhsY ) {
+            @{ $self->@{vel} }[1]--;
+            @{ $obj->@{vel} }[1]++;
+        }
+        else {
+            @{ $self->@{vel} }[1]++;
+            @{ $obj->@{vel} }[1]--;
+        }
+
+        if ( $lhsZ > $rhsZ ) {
+            @{ $self->@{vel} }[2]--;
+            @{ $obj->@{vel} }[2]++;
+        }
+        else {
+            @{ $self->@{vel} }[2]++;
+            @{ $obj->@{vel} }[2]--;
+        }
+
+    }
+    my @vel = @{ $self->@{vel} };
+    @{ $self->@{pos} }[0] += $vel[0];
+    @{ $self->@{pos} }[1] += $vel[1];
+    @{ $self->@{pos} }[2] += $vel[2];
 }
