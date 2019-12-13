5001f3419f6e1b9413ebe329881ae02322185249
Will Cass <will_cass@hotmail.co.uk>
2019-12-13 18:48:13 +0000

    We calculate the example, but not the part 2

--- 2019/day12/day12.pl
+++ 2019/day12/day12.pl
@@ -1,7 +1,8 @@
 use strict;
 use warnings;
 use AdventOfCode::Planet;
-
+use List::Util 'first';
+use Math::Utils 'lcm';
 # <x=8, y=0, z=8>
 # <x=0, y=-5, z=-10>
 # <x=16, y=10, z=-5>
@@ -19,14 +20,14 @@ my $D = AdventOfCode::Planet->new(19,-10,-7);
 # my $C = AdventOfCode::Planet->new( 4,  -8,  8 );
 # my $D = AdventOfCode::Planet->new( 3,  5,   -1 );
 my @otherPlanets = ( $A, $B, $C, $D );
-print "After 0 steps:\n";
-for (@otherPlanets) {
-    $_->printOut();
-}
-print "\n";
+# print "After 0 steps:\n";
+# for (@otherPlanets) {
+    # $_->printOut();
+# }
+# print "\n";
 for my $index ( 1 .. 1000 ) {
     my @temp = @otherPlanets;
-    print "After $index steps:\n";
+    # print "After $index steps:\n";
     shift @temp;
     $A->update( \@temp );
     shift @temp;
@@ -35,14 +36,58 @@ for my $index ( 1 .. 1000 ) {
     $C->update( \@temp );
     shift @temp;
     $D->update( \@temp );
-    for (@otherPlanets) {
-        $_->printOut();
-    }
-    print "\n";
+    # for (@otherPlanets) {
+        # $_->printOut();
+    # }
+    # print "\n";
 }
 my $total = 0;
 for my $planet (@otherPlanets){
     $total += $planet->getEnergy();
 }
 
-print "[*] Total Energy: $total\n";
+print "[*] Part1 - Total Energy: $total\n";
+
+# $A = AdventOfCode::Planet->new(8,0,8);
+# $B = AdventOfCode::Planet->new(0,-5,-10);
+# $C = AdventOfCode::Planet->new(16,10,-5);
+# $D = AdventOfCode::Planet->new(19,-10,-7);
+$A = AdventOfCode::Planet->new( -1, 0,   2 );
+$B = AdventOfCode::Planet->new( 2,  -10, -7 );
+$C = AdventOfCode::Planet->new( 4,  -8,  8 );
+$D = AdventOfCode::Planet->new( 3,  5,   -1 );
+@otherPlanets = ( $A, $B, $C, $D );
+
+my @states;
+my $steps = 0;
+my @factors;
+while(1){
+    my @temp = @otherPlanets;
+    # print "After $index steps:\n";
+    shift @temp;
+    $A->update( \@temp );
+    shift @temp;
+    $B->update( \@temp );
+    shift @temp;
+    $C->update( \@temp );
+    shift @temp;
+    $D->update( \@temp );
+
+    my $checksum = $A->getCheckSum() . $B->getCheckSum() . $C->getCheckSum() . $D->getCheckSum();
+    
+    my @match = grep{/$checksum/} @states;
+    
+    if (@match > 0){
+        push @factors, $steps;
+        last;
+    }else{
+        push @states, $checksum;
+    }
+    
+    # last;
+    $steps++;
+}
+
+
+my $factor = lcm(@factors);
+print "[*] $factors[0]\n";
--- 2019/lib/AdventOfCode/Planet.pm
+++ 2019/lib/AdventOfCode/Planet.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Exporter qw(import);
 
-our @EXPORT_OK = qw(new update printOut getEnergy);
+our @EXPORT_OK = qw(new update printOut getEnergy getCheckSum);
 
 sub new {
     my $class = shift;
@@ -26,6 +26,37 @@ sub new {
     return $self;
 }
 
+sub getCheckSum {
+    my $self = shift;
+    my @pos  = @{ $self->@{pos} };
+    my @vel  = @{ $self->@{vel} };
+    return "$pos[0]$pos[1]$pos[2]$vel[0]$vel[1]$vel[2]";
+}
+
+sub getCheckSumX {
+    my $self = shift;
+    my @pos  = @{ $self->@{pos} };
+    my @vel  = @{ $self->@{vel} };
+    return "$pos[0]$vel[0]";
+}
+
+
+sub getCheckSumY {
+    my $self = shift;
+    my @pos  = @{ $self->@{pos} };
+    my @vel  = @{ $self->@{vel} };
+    return "$pos[1]$vel[1]";
+}
+
+
+sub getCheckSumZ {
+    my $self = shift;
+    my @pos  = @{ $self->@{pos} };
+    my @vel  = @{ $self->@{vel} };
+    return "$pos[2]$vel[2]";
+}
+
+
 sub getEnergy {
     my $self = shift;
     my @pos = @{$self->@{pos}};
@@ -40,6 +71,81 @@ sub printOut {
     my @vel  = @{ $self->@{vel} };
     print
 "pos=<x=$pos[0], y=$pos[1], z=$pos[2]>, vel=<x=$vel[0], y=$vel[1], z=$vel[2]>\n";
+}
+
+sub updateX {
+    my $self = shift;
+    my @otherPlanets = @{ $_[0] };
+    my $lhsX = @{ $self->@{pos} }[0];
+    for (@otherPlanets) {
+        my $obj = $_;
+        if ( $self == $obj ) {
+            next;
+        }
+        my $rhsX = @{ $obj->@{pos} }[0];
+
+        # my @objvel = @{$obj->@{vel}};
+        if ( $lhsX > $rhsX ) {
+            @{ $self->@{vel} }[0]--;
+            @{ $obj->@{vel} }[0]++;
+        }
+        elsif($lhsX <= $rhsX){
+            @{ $self->@{vel} }[0]++;
+            @{ $obj->@{vel} }[0]--;
+        }
+    }
+    my @vel = @{ $self->@{vel} };
+    @{ $self->@{pos} }[0] += $vel[0];
+}
+
+sub updateY {
+    my $self = shift;
+    my @otherPlanets = @{ $_[0] };
+    my $lhsY = @{ $self->@{pos} }[1];
+    for (@otherPlanets) {
+        my $obj = $_;
+        if ( $self == $obj ) {
+            next;
+        }
+        my $rhsY = @{ $obj->@{pos} }[1];
+
+        # my @objvel = @{$obj->@{vel}};
+        if ( $lhsY > $rhsY ) {
+            @{ $self->@{vel} }[1]--;
+            @{ $obj->@{vel} }[1]++;
+        }
+        elsif($lhsY <= $rhsY){
+            @{ $self->@{vel} }[1]++;
+            @{ $obj->@{vel} }[1]--;
+        }
+    }
+    my @vel = @{ $self->@{vel} };
+    @{ $self->@{pos} }[1] += $vel[1];
+}
+
+sub updateZ {
+    my $self = shift;
+    my @otherPlanets = @{ $_[0] };
+    my $lhsZ = @{ $self->@{pos} }[2];
+    for (@otherPlanets) {
+        my $obj = $_;
+        if ( $self == $obj ) {
+            next;
+        }
+        my $rhsZ = @{ $obj->@{pos} }[2];
+
+        # my @objvel = @{$obj->@{vel}};
+        if ( $lhsZ > $rhsZ ) {
+            @{ $self->@{vel} }[2]--;
+            @{ $obj->@{vel} }[2]++;
+        }
+        elsif($lhsZ <= $rhsZ){
+            @{ $self->@{vel} }[2]++;
+            @{ $obj->@{vel} }[2]--;
+        }
+    }
+    my @vel = @{ $self->@{vel} };
+    @{ $self->@{pos} }[2] += $vel[2];
 }
 
 sub update {
