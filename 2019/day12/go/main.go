c362e8b0a76f03120c9a986dd273e216486baf97
Will Cass <will_cass@hotmail.co.uk>
2019-12-15 22:12:12 +0000

    Cleaning up the code

--- 2019/day12/go/main.go
+++ 2019/day12/go/main.go
@@ -5,30 +5,24 @@ import (
 	"sync"
 )
 
+// Actual Input
 // <x=8, y=0, z=8>
 // <x=0, y=-5, z=-10>
 // <x=16, y=10, z=-5>
 // <x=19, y=-10, z=-7>
 
+// Testing input
 // <x=-1, y=0, z=2>
 // <x=2, y=-10, z=-7>
 // <x=4, y=-8, z=8>
 // <x=3, y=5, z=-1>
 func main() {
-	// A := NewPlanet(8, 0, 8)
-	// B := NewPlanet(0, -5, -10)
-	// C := NewPlanet(16, 10, -5)
-	// D := NewPlanet(19, -10, -7)
 	Part1()
 	Part2()
 
 }
 
 func Part2() {
-	// A := NewPlanet(-1, 0, 2)
-	// B := NewPlanet(2, -10, -7)
-	// C := NewPlanet(4, -8, 8)
-	// D := NewPlanet(3, 5, -1)
 	var wg sync.WaitGroup
 	var A, B, C int
 	wg.Add(1)
@@ -60,7 +54,6 @@ func X() int {
 	D := NewPlanet(19, -10, -7)
 	otherPlanets := []*Planet{A, B, C, D}
 	steps := 0
-	// xmatch, ymatch, zmatch := false, false, false
 	check := A.CheckSum() + B.CheckSum() + C.CheckSum() + D.CheckSum()
 	for {
 		A.UpdateX(otherPlanets[1:])
@@ -68,7 +61,6 @@ func X() int {
 		C.UpdateX(otherPlanets[3:])
 		D.UpdateX(otherPlanets[4:])
 		checksum := A.CheckSum() + B.CheckSum() + C.CheckSum() + D.CheckSum()
-		// return steps
 		if check == checksum {
 			return steps
 		}
@@ -83,7 +75,6 @@ func Y() int {
 	D := NewPlanet(19, -10, -7)
 	otherPlanets := []*Planet{A, B, C, D}
 	steps := 0
-	// xmatch, ymatch, zmatch := false, false, false
 	check := A.CheckSum() + B.CheckSum() + C.CheckSum() + D.CheckSum()
 	for {
 		A.UpdateY(otherPlanets[1:])
@@ -106,8 +97,6 @@ func Z() int {
 	D := NewPlanet(19, -10, -7)
 	otherPlanets := []*Planet{A, B, C, D}
 	steps := 0
-	// xmatch, ymatch, zmatch := false, false, false
-	// states := []string{}
 	check := A.CheckSum() + B.CheckSum() + C.CheckSum() + D.CheckSum()
 	for {
 		A.UpdateZ(otherPlanets[1:])
@@ -119,7 +108,6 @@ func Z() int {
 		if check == checksum {
 			return steps
 		}
-		// states = append(states, checksum)
 		steps++
 	}
 }
