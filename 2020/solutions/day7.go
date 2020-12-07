f1b0a8f88ed92bb3c1b611e3321b70a88c899857
Will Cass <will_cass@hotmail.co.uk>
2020-12-07 18:15:38 +0000

    Day 7 p1 and p2 done

--- 2020/solutions/day7.go
+++ 2020/solutions/day7.go
@@ -10,7 +10,8 @@ import (
 )
 
 type Day7 struct {
-	Title string
+	Title   string
+	bagTree map[string]*entities.Bag
 }
 
 func NewDay7() *Day7 {
@@ -20,14 +21,61 @@ func NewDay7() *Day7 {
 func (d Day7) Run() {
 	input := helpers.GetInput("day7.txt")
 	inputData := strings.Split(input, "\r\n")
-	d.part1(inputData)
+	d.bagTree = d.buildBagTree(inputData)
+	d.part1()
+	d.part2()
 }
 
 func (d Day7) GetTitle() string {
 	return d.Title
 }
 
-func (d Day7) part1(data []string) {
+func (d Day7) part2() {
+	count := 0
+	shinyGoldBag := d.bagTree["shiny gold"]
+	count = d.bagSearchPart2(shinyGoldBag)
+	fmt.Printf("[*] Shiny Bag contains %d bags\n", count)
+}
+
+func (d Day7) part1() {
+	count := 0
+	for _, bag := range d.bagTree {
+		if bag.Children != nil {
+			if d.bagSearchPart1(bag) {
+				count++
+			}
+		}
+	}
+	fmt.Printf("[*] Bags Containing Shiny Gold: %d\n", count)
+}
+
+func (d Day7) bagSearchPart2(bag *entities.Bag) int {
+	count := len(bag.Children)
+	for _, child := range bag.Children {
+		count += d.bagSearchPart2(child)
+	}
+	return count
+}
+
+func (d Day7) bagSearchPart1(bag *entities.Bag) bool {
+	skipDupesColour := ""
+	for _, bag := range bag.Children {
+		if bag.Colour == "shiny gold" {
+			return true
+		} else {
+			if bag.Colour != skipDupesColour {
+				skipDupesColour = bag.Colour
+
+				if d.bagSearchPart1(bag) {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func (d Day7) buildBagTree(data []string) map[string]*entities.Bag {
 	bagHash := make(map[string]*entities.Bag)
 	for _, rule := range data {
 		bagNameR, _ := regexp.Compile("^([a-z ]+) b")
@@ -51,6 +99,5 @@ func (d Day7) part1(data []string) {
 			}
 		}
 	}
-	test := bagHash["vibrant blue"]
-	fmt.Println(test)
+	return bagHash
 }
